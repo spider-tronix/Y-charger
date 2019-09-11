44723a6495b476916d9261bf96e5e623983c4136
KrispyCamel4u <neerajjangra4u@gmail.com>
2019-09-11 16:55:42 +0530

    Correction

--- findings/TINA Modified Sine wave signal/programmer.cpp
+++ findings/TINA Modified Sine wave signal/programmer.cpp
@@ -3,17 +3,16 @@
 #include<string>
 using namespace std;
 int main(){
-	double f=200,T,A=20;
+	double f=500,T,A=20;
 	//cout<<"Enter the frequency:";
 	//cin>>f;
 	T=1/f;
-	T/=4;
 	ofstream outfile;
 	outfile.open("Modified_sine_wave.txt");
 	outfile<<"REPEAT FOREVER"<<endl;
 	outfile<<0<<" "<<0<<endl;
 	outfile<<T/8<<" "<<0<<endl;
-	cout<<T/8;
+	cout<<T;
 	outfile<<(T/8)<<" "<<A<<endl;
 	outfile<<(T*(3.0/8))<<" "<<A<<endl;
 	outfile<<(T*(3.0/8))<<" "<<0<<endl;
